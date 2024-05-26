14ce1e3ab03e6479763408bae33f585c2ede2626
Radic Ivo <89729414+PROPHECY55@users.noreply.github.com>
2024-05-26 19:48:59 +0200

    Added ToDo+function logic

--- src/string_calculator.js
+++ src/string_calculator.js
@@ -1,28 +1,18 @@
-// Funktion, die ein Array mit 4 bis 6 Zufallszahlen von 1 bis 6 erzeugt
-function generateRandomArray() {
-    const length = Math.floor(Math.random() * 3) + 4; // Länge zwischen 4 und 6
-    const array = [];
-    for (let i = 0; i < length; i++) {
-        array.push(Math.floor(Math.random() * 6) + 1); // Zahl zwischen 1 und 6
-    }
-    return array;
-}
+// Electrons around the Cores = "Petals Around the Rose" Kata
+// https://en.wikipedia.org/wiki/Petals_Around_the_Rose
 
-// Funktion, die das Array auswertet und die Summe der spezifischen Regeln berechnet
-function calculateSum(array) {
-    let sum = 0;
-    for (const number of array) {
-        if (number === 3) {
-            sum += 2;
-        } else if (number === 5) {
-            sum += 4;
-        }
-    }
-    return sum;
-}
+// Zahlen bekommen bestimmte Werte, Solution:
+// 1 = 0
+// 2 = 0
+// 3 = 2
+// 4 = 0
+// 5 = 4
+// 6 = 0
 
-// Hauptlogik
-const array = generateRandomArray();
-console.log('Array:', array);
-const result = calculateSum(array);
-console.log('Summe der spezifischen Regeln:', result);
+function calculateSum(arr){
+    const lookup = [0, 0, 2, 0, 4, 0];
+
+    return arr.reduce((acc, num) => {
+        return acc + parseInt(num - 1);
+    }, 0);
+}
